9d1e9826b6ab7ddee0b618a8629ddea0849bbd93
Sam Rogers <samrogers15@gmail.com>
2021-04-03 19:22:42 -0700

    Updating App.js file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './Components/NavBar';
-import Title from './Components/Title';
-import SearchBar from './Components/SearchBar';
-import Table from './Components/EmployeeTable';
-import Footer from './Components/Footer';
-
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar";
+import JumboTron from "./Components/JumboTron";
+import SearchBar from "./Components/SearchBar";
+import EmployeeTable from "./Components/EmployeeTable";
+import "./App.css";
 
 function App() {
+  const [search, setSearchState] = useState("");
   return (
     <>
-    <NavBar />
-    <Title />
-    <SearchBar />
-    <Table />
-    <Footer />
+      <NavBar />
+      <JumboTron>
+        <SearchBar search={search} setSearch={setSearchState} />
+      </JumboTron>
+      <EmployeeTable search={search} />
     </>
   );
 }
