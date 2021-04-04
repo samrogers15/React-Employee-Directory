da97cc764f630571a795c7bd9a671d7e7ebabdf9
Sam Rogers <samrogers15@gmail.com>
2021-04-03 18:52:08 -0700

    Updating components within App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import Title from './Components/Title';
+import SearchBar from './Components/SearchBar';
+import Table from './Components/EmployeeTable';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar />
+    <Title />
+    <SearchBar />
+    <Table />
+    <Footer />
+    </>
   );
 }
 
