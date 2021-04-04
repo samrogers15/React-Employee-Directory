f9715fba27df9360239c6860c89a8738283e1395
Sam Rogers <samrogers15@gmail.com>
2021-04-03 19:23:28 -0700

    Updated JumboTron component

---
+++ src/Components/JumboTron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Jumbotron } from 'react-bootstrap';
+
+const JumboTron = () => {
+  return (
+    <Jumbotron>
+      <h1>Sam Rogers LLC Employee Directory</h1>
+      <p>
+        Welcome to the employee directory for Sam Rogers LLC. This application was designed for a member of the HR team to easily search information on employees in the company. Utilize the search bar below to search for a specific employee or use the table to sort employees by a specific field.
+      </p>
+    </Jumbotron>
+  );
+};
+
+export default JumboTron;
